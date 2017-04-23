0569d79b3d29cb88bef4fa1f7159f70f8524df63
starshipped <cpalejandro@up.edu.ph>
2017-04-23 13:53:44 +0700

    Update Food.java

--- Food.java
+++ Food.java
@@ -2,8 +2,18 @@ package FoodObjects;
 
 public class Food{
   private int nutrients;
+  private int loveGained;
+  private int cost;
   
   public int getNutrients(){
     return this.nutrients;
+  }
+  
+  public int getLove(){
+    return this.loveGained; 
+  }
+  
+  public int getCost(){
+    return this.cost;
   }
 }
