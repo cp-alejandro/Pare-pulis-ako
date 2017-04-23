eb9d2145b05f041b26593380e07f16024c5d6b78
starshipped <cpalejandro@up.edu.ph>
2017-04-23 13:01:40 +0700

    Create Boo.java

---
+++ Boo.java
@@ -0,0 +1,16 @@
+public class Boo{
+  /*STATS*/
+  private int level; //in game max level = 60
+  private int energy; //max 50 points
+  private int hunger; //max 30 points?
+  private int love; //max 20 points?
+  
+  /*APPEARANCE*/
+  public Boo{
+    this.level = 1;
+    this.energy = 30;
+    this.hunger = 15;
+    this.love = 10;
+    
+  }
+}
